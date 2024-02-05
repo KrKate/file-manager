29aa2f5334ab798053f5f040c45691d888e62b20
Ekaterina Kiryanova <109188496+KrKate@users.noreply.github.com>
2024-02-06 00:31:09 +0500

    feat: add add function

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import { readdir, stat } from 'fs/promises';
 import { createReadStream } from 'fs';
 import { fileURLToPath } from 'url';
 import {ls} from './ls.js'
+import {cat} from './cat.js'
+import {add} from './add.js'
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -20,50 +22,6 @@ const rl = readline.createInterface({
   prompt: '> '
 });
 
-
-// const ls = async () => {
-//   try {
-//     const files = await readdir(__dirname, { withFileTypes: true });
-//     const fileStats = [];
-
-//     const folders = files.filter(file => file.isDirectory());
-//     const regularFiles = files.filter(file => file.isFile());
-
-//     folders.sort((a, b) => a.name.localeCompare(b.name));
-//     regularFiles.sort((a, b) => a.name.localeCompare(b.name));
-
-//     const sortedFiles = folders.concat(regularFiles);
-//     for (const file of sortedFiles) {
-//       const filePath = join(__dirname, file.name);
-//       const stats = await stat(filePath);
-//       const type = stats.isDirectory() ? 'Folder' : 'File';
-//       fileStats.push({ Name: file.name, Type: type });
-//     }
-
-//     console.table(fileStats);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
-
-function cat(filePath) {
-  return new Promise((resolve, reject) => {
-    const readable = createReadStream(filePath);
-
-    readable.on('data', (chunk) => {
-      process.stdout.write(chunk.toString());
-    });
-
-    readable.on('error', (error) => {
-      reject(error);
-    });
-
-    readable.on('end', () => {
-      resolve();
-    });
-  });
-}
-
 rl.prompt();
 rl.on('line', async (input) => {
   input = input.trim();
@@ -75,7 +33,8 @@ rl.on('line', async (input) => {
           throw new Error('Invalid input');
         }
         rl.close();
-        break;
+      break;
+
       case 'cd':
         if (args.length === 0) {
           throw new Error('Invalid input');
@@ -85,26 +44,36 @@ rl.on('line', async (input) => {
           throw new Error('Operation failed');
         }
         __dirname = newPath;
-        break;
+      break;
+
       case 'up':
         if (args.length > 0) {
           throw new Error('Invalid input');
         }
         __dirname = dirname(__dirname);
-        break;
+      break;
+
       case 'ls':
         if (args.length > 0) {
           throw new Error('Invalid input');
         }
         await ls();
-        break;
+      break;
+
       case 'cat':
         if (args.length !== 1) {
           throw new Error('Invalid input');
         }
         const filePath = join(__dirname, args[0]);
         await cat(filePath);
-        break;
+      break;
+
+      case 'add':
+          if (args.length !== 1) {
+            throw new Error('Invalid input');
+          }
+          await add(args[0]);
+      break;
     }
   } catch (err) {
     console.log(err.message);
